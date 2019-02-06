d0dc60cd2e17fab62422313d0e1d2db422f5622b
DuyDatNgoUET <DuyDatNgoUET@users.noreply.github.com>
2019-02-06 13:40:18 +0700

    first_test

---
+++ Test_onclass/src/run.java
@@ -0,0 +1,7 @@
+public class run {
+    public static void main(String[] args){
+        test C = new test();
+        System.out.println(C.getValue());
+        System.out.println(C.increase());
+    }
+}
---
+++ Test_onclass/src/test.java
@@ -0,0 +1,11 @@
+public class test {
+    private int c = 0;
+    public test increase() {
+        c++;
+        return this;
+    }
+    public int getValue() {
+        return c;
+    }
+
+}
