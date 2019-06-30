6cb2e14473a0a189085ebed186b00a75240bbd5f
DuyDatNgoUET <DuyDatNgoUET@users.noreply.github.com>
2019-06-30 22:24:18 +0700

    week4 + 5
    week 4 : complete
    week5 : 1/2

--- Week4/src/test/java/testMaxArray.java
+++ Week4/src/test/java/testMaxArray.java
@@ -1,6 +1,8 @@
 import junit.framework.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class testMaxArray {
 
     @Test
@@ -8,7 +10,16 @@ public class testMaxArray {
         int mang[] = {1,2,3,4};
         int maxarray = 4;
         int a =arrayMax.Max(mang);
-        Assert.assertEquals(maxarray,a);
+        assertEquals(maxarray,a);
 
+    }
+
+    @Test
+    public void Test2(){
+        int mang[] = {1,5,3,36,2,4};
+        int maxMang = 36;
+        int a = arrayMax.Max(mang);
+
+        assertEquals(maxMang,a);
     }
 }
