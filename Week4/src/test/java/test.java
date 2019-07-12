b2a140d18a05b9a6815d9ec62646a427c3c67636
DuyDatNgoUET <DuyDatNgoUET@users.noreply.github.com>
2019-07-12 11:20:42 +0700

    Delete week4

--- Week4/src/test/java/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-        public static void main(String []args){
-            int a[]= {1,23,35,65,765,45423,4236,5474,6565,858,587675};
-            int result = arrayMax.Max(a);
-
-            System.out.println(result);
-        }
-}
