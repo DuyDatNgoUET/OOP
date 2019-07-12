da4442a3bb2e603243a21c7c501b675e964ab6f3
DuyDatNgoUET <DuyDatNgoUET@users.noreply.github.com>
2019-07-12 10:20:33 +0700

    PS + UCLN

--- week3/src/PS/Fraction.java
+++ week3/src/PS/Fraction.java
@@ -1,15 +1,18 @@
+package PS;
 
 
 public class Fraction {
-    private int numerator ;
+    private int numerator;
     private int denominator;
-
-    public Fraction() {
-    }
 
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
+        if (denominator == 0) return;
         this.denominator = denominator;
+    }
+
+    public void Print() {
+        System.out.println(this.numerator + "/" + this.denominator);
     }
 
     public int getNumerator() {
@@ -25,38 +28,85 @@ public class Fraction {
     }
 
     public void setDenominator(int denominator) {
+        if (denominator == 0) return;
         this.denominator = denominator;
     }
 
-
-    public int reduce(int a, int b){
-        if(a==0 || b==0) return a+b;
-        else if(a>b) return reduce(a-b,b);
-        return reduce(a,b-a);
+    public int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 
-    public void add(Fraction p1, Fraction p2){
-        this.numerator = p1.numerator*p2.denominator + p2.numerator*p1.denominator;
-        this.denominator = p1.denominator*p2.denominator;
+    public Fraction() {
     }
 
-    public void subtract(Fraction p1, Fraction p2){
-        this.numerator = p1.numerator*p2.denominator - p2.numerator*p1.denominator;
-        this.denominator = p1.denominator*p2.denominator;
+    public Fraction reduce() {
+        int gcd = gcd(this.numerator, this.denominator);
+        if (this.denominator != 0 && this.numerator != 0) {
+            this.setNumerator(numerator / gcd);
+            this.setDenominator(denominator / gcd);
+        } else {
+            this.setNumerator(numerator);
+            this.setDenominator(denominator);
+        }
+
+        return this;
     }
 
-    public void multiply(Fraction p1, Fraction p2){
-        this.numerator = p1.numerator*p2.numerator;
-        this.denominator = p1.denominator*p2.denominator;
+
+    public Fraction add(Fraction other) {
+        Fraction tong = new Fraction();
+        if (this.getDenominator() == 0 || other.getDenominator() == 0) return null;
+        tong.setNumerator(this.getNumerator() * other.getDenominator() + this.getDenominator() * other.getNumerator());
+        if (this.getDenominator() == 0 || other.getDenominator() == 0) return null;
+        tong.setDenominator(this.getDenominator() * other.getDenominator());
+        tong.reduce();
+        return tong;
     }
 
-    public void devide(Fraction p1, Fraction p2){
-        this.numerator = p1.numerator*p2.denominator;
-        this.denominator = p1.denominator*p2.numerator;
+    public Fraction subtract(Fraction other) {
+        Fraction hieu = new Fraction();
+        if (this.getDenominator() == 0 || other.getDenominator() == 0) return null;
+        hieu.setNumerator(this.getNumerator() * other.getDenominator() - this.getDenominator() * other.getNumerator());
+        if (this.getDenominator() == 0 || other.getDenominator() == 0) return null;
+        hieu.setDenominator(this.getDenominator() * other.getDenominator());
+        hieu.reduce();
+        return hieu;
     }
 
-    public boolean equals(Object obj ){
-        return false;
+    public Fraction multifly(Fraction other) {
+        Fraction tich = new Fraction();
+
+        tich.setNumerator(this.getNumerator() * other.getNumerator());
+        if (this.getDenominator() == 0 || other.getDenominator() == 0) return null;
+        tich.setDenominator(this.getDenominator() * other.getDenominator());
+        tich.reduce();
+        return tich;
+    }
+
+    public Fraction divide(Fraction other) {
+        Fraction thuong = new Fraction();
+
+        thuong.setNumerator(this.getNumerator() * other.getDenominator());
+        if (other.getNumerator() == 0) return null;
+        thuong.setDenominator(this.getDenominator() * other.getNumerator());
+
+        thuong.reduce();
+        return thuong;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Fraction)) return false;
+        Fraction other = (Fraction) obj;
+        return this.numerator * other.denominator == other.numerator * this.denominator;
+    }
+
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(3, 5);
+        Fraction f2 = new Fraction(1, 2);
+        Fraction f3 = f1.multifly(f2);
+        f3.Print();
 
     }
 }
--- week3/src/neon/exercise1/UCLN.java
+++ week3/src/neon/exercise1/UCLN.java
@@ -5,18 +5,14 @@ import java.util.Scanner;
 //import your library
 public class UCLN {
     public static int gcd(int a, int b){
-        if(a==0 || b==0){
-            return a+b;
-        }
-        if(a>b) return gcd(a-b,b);
-        return gcd(a,b-a);
+        if(b==0) return a;
+        return gcd(b,a%b);
     }
 
     public static void main(String[] args){
         Scanner sc1 = new Scanner(System.in);
         int s1 = sc1.nextInt();
-        Scanner sc2 = new Scanner(System.in);
-        int s2 = sc2.nextInt();
+        int s2 = sc1.nextInt();
 
         int result = UCLN.gcd(s1,s2);
         System.out.println(result);
